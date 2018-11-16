2b3274acfd7bcb03e09bf3c257533e81231529e5
kapzam <44903900+kapzam@users.noreply.github.com>
2018-11-16 10:43:11 +0300

    ekrandansayiokuma java

---
+++ ekrandansayiokuma.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Program {
+public static void main(String[] args) {
+Scanner yaz = new Scanner(System.in);
+System.out.println("Sayı giriniz");
+int girilensayi = yaz.nextInt();
+System.out.println("Girilen Sayı"+ girilensayi);
+}
+}
